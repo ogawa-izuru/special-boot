40da93d577182f5feb0ab18b8038214c11a0c484
小川 洸 <ogawa_izuru@sunm.local>
2020-07-06 17:27:58 +0900

    一覧画面実装

--- my-app/src/app/blog-list/blog-list.component.ts
+++ my-app/src/app/blog-list/blog-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogDetailService } from '../service/blog-detail.service';
 
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
-  styleUrls: ['./blog-list.component.scss']
+  styleUrls: ['./blog-list.component.scss'],
 })
 export class BlogListComponent implements OnInit {
+  articleList;
 
-  constructor() { }
+  constructor(private detail: BlogDetailService) {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.articleList = await this.detail
+      .getAllArticle()
+      .then((response) => response);
+    this.articleList = this.articleList.sort(function (a, b) {
+      if (a.id < b.id) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
   }
-
 }
