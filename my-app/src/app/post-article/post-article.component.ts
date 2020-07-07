20ecb8b3b1aeace9c9656ed4546ad7578d91de72
安田 直也 <yasuda_naoya@sunm.local>
2020-07-07 13:14:30 +0900

    detailをPostArticleServiceに名前変更

--- my-app/src/app/post-article/post-article.component.ts
+++ my-app/src/app/post-article/post-article.component.ts
@@ -13,21 +13,22 @@ export class PostArticleComponent implements OnInit {
   mainContent: string;
   isError: boolean = false;
 
-  constructor(private detail: PostArticleService, private router: Router) {}
+  constructor(
+    private PostArticleService: PostArticleService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   postArticle(): void {
     (this.title &&
       this.mainContent &&
-      this.detail
-        .postArticle({
-          title: this.title,
-          mainContent: this.mainContent,
-          good: 0,
-          bad: 0,
-        })
-        .subscribe(() => this.router.navigate(['/']))) ||
+      this.PostArticleService.postArticle({
+        title: this.title,
+        mainContent: this.mainContent,
+        good: 0,
+        bad: 0,
+      }).subscribe(() => this.router.navigate(['/']))) ||
       (this.isError = true);
   }
 }
